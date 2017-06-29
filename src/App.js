eb3238c0a8aa4b15331a031d7d2d3a0aa97ef9c7
Greg Lazarev <greg@thoughtbot.com>
2017-06-28 20:54:02 -0700

    Implement initial feature and test

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider, connect } from "react-redux";
+import { createStore } from "redux";
+import axios from "axios";
+import "./App.css";
+
+function reducer(state = {}, action) {
+  switch (action.type) {
+    case "SET_DATA":
+      return {
+        ...state,
+        data: action.data
+      };
+    default:
+      return state;
+  }
+}
+
+function setData(data) {
+  return { type: "SET_DATA", data: data };
+}
+
+function fetchData() {
+  return axios.get("http://echo.jsontest.com/key/value/one/two");
+}
+
+function getData(dispatch) {
+  return () => fetchData().then(response => dispatch(setData(response.data)));
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={createStore(reducer)}>
+        <Router>
+          <Route path="/" component={WelcomePage} />
+        </Router>
+      </Provider>
+    );
+  }
+}
+
+class Welcome extends Component {
+  componentDidMount() {
+    this.props.getData();
+  }
+
+  render() {
+    if (this.props.data) {
+      return <div>We have data!</div>;
+    } else {
+      return <div>Waiting for data...</div>;
+    }
+  }
+}
+
+const WelcomePage = connect(
+  state => {
+    return {
+      data: state.data
+    };
+  },
+  dispatch => {
+    return {
+      getData: getData(dispatch)
+    };
+  }
+)(Welcome);
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import axios from "axios";
+
+it("renders without crashing", async () => {
+  axios.get = jest.fn(() => {
+    return Promise.resolve({ data: { one: 1, two: 2 } });
+  });
+  const div = document.createElement("div");
+  const component = await ReactDOM.render(<App />, div);
+
+  expect(div.textContent).toEqual("We have data!");
+});
